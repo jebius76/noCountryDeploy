a6eb1397d539aea96d132be68da6957fe96f0251
JEB <jebius76@gmail.com>
2023-09-13 17:47:59 -0300

    config: Clonado repo original.

--- src/main/java/com/c1331tjava/ServiceApp/dto/client/BidProviderDTO.java
+++ src/main/java/com/c1331tjava/ServiceApp/dto/client/BidProviderDTO.java
@@ -1,16 +1,14 @@
-package com.c1331tjava.ServiceApp.dto.provider;
+package com.c1331tjava.ServiceApp.dto.client;
 
 import com.c1331tjava.ServiceApp.model.Area;
 import lombok.Data;
-
 import java.util.List;
 
 @Data
-public class ProviderDTO {
+public class BidProviderDTO {
+    private Long id;
     private String imageLink;
     private String userName;
     private String userLastname;
-    private String email;
-    private String te;
     private List<Area> areas;
 }
